5c188528e8870c826b42c540b9a4c21caf421212
Jeroen Maes <jeroen.maes@LIVE.COM>
2019-02-13 17:52:00 +0100

    Remove old ASP.NET project

---
+++ src/EpMon.Web.Core/EpMon.Web.Core/Controllers/ConfigurationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EpMon.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpMon.Web.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly EpMonRepository _repo;
+        public ConfigurationController()
+        {
+            _repo = new EpMonRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _repo.GetEndpoints().Select(x => x.Url);
+        }
+
+        [HttpPost]
+        public void Post(string url, int checkInterval, string tags)
+        {
+            int id = _repo.AddEndpoint(url, checkInterval, tags);
+        }
+    }
+}
--- src/EpMon.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EpMon.Web.MvcApplication" Language="C#" %>
