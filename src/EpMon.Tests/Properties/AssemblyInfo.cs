1de5884ba6cfc7501b9c42abdfd3bb6c1479220e
Jeroen Maes <jeroen.maes@LIVE.COM>
2019-02-13 10:21:56 +0100

    Migrate to VS2017 project format

--- src/EpMon.Tests/Properties/AssemblyInfo.cs
+++ src/EpMon.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("EpMon.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EpMon.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("9ae5d942-73fb-45fa-8d12-2f542fcdd09c")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
