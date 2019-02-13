b1536f8244448fe9ca0ce6dc419ed880188c754a
Jeroen Maes <jeroen.maes@LIVE.COM>
2019-02-13 15:15:53 +0100

    Implement appsettings so upgrade Monitor project to NetCoreApp2.1

--- src/EpMon.Data/EpMonContext.cs
+++ src/EpMon.Data/EpMonContext.cs
@@ -1,21 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Configuration;
+
 using System.Linq;
 using EpMon.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EpMon.Data
 {
     public class EpMonContext : DbContext
     {
+        private static string _connectionString;
+
         public EpMonContext() : base()
         {
+            LoadConnectionString();
             Database.Migrate();
         }
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["EpMonContext"].ConnectionString);
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        private static void LoadConnectionString()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", false);
+
+            var configuration = builder.Build();
+
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
 
         public DbSet<Endpoint> Endpoints { get; set; }
