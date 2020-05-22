fa5c9546ddc597113471c31295671605ac61192f
Jeroen Maes <jeroen.maes.external@arcelormittal.com>
2020-05-22 17:33:03 +0200

    Update packages

--- src/EpMon.Web/Extensions/IServiceCollectionExtensions.cs
+++ src/EpMon.Web/Extensions/IServiceCollectionExtensions.cs
@@ -12,23 +12,35 @@ namespace EpMon.Web.Extensions
         public static IServiceCollection AddScheduledJobs(this IServiceCollection services)
         {
             var serviceProvider = services.BuildServiceProvider();
-            var endpointService = serviceProvider.GetService<EndpointService>();
-            var endpoints = endpointService.GetAllActiveEndpoints();
-            
-            foreach (var endpoint in endpoints)
+
+            try
             {
+                var endpointService = serviceProvider.GetService<EndpointService>();
+                var endpoints = endpointService.GetAllActiveEndpoints();
+
+                foreach (var endpoint in endpoints)
+                {
+                    services.AddScheduler(builder =>
+                    {
+                        builder.AddJob(provider => new EndpointJob(provider, endpoint));
+                        builder.UnobservedTaskExceptionHandler = (sender, exceptionEventArgs) => UnobservedJobHandlerHandler(exceptionEventArgs, services);
+                    });
+                }
+
                 services.AddScheduler(builder =>
                 {
-                    builder.AddJob(provider => new EndpointJob(provider, endpoint));
+                    builder.AddJob(provider => new MaintenanceJob(provider));
                     builder.UnobservedTaskExceptionHandler = (sender, exceptionEventArgs) => UnobservedJobHandlerHandler(exceptionEventArgs, services);
                 });
             }
-
-            services.AddScheduler(builder =>
+            catch (Exception e)
             {
-                builder.AddJob(provider => new MaintenanceJob(provider));
-                builder.UnobservedTaskExceptionHandler = (sender, exceptionEventArgs) => UnobservedJobHandlerHandler(exceptionEventArgs, services);
-            });
+                var logger = services.FirstOrDefault(service => service.ServiceType == typeof(ILogger));
+                var loggerInstance = (ILogger)logger?.ImplementationInstance;
+                loggerInstance?.LogCritical(e, "Exception in AddScheduledJobs");
+
+                Console.WriteLine("ERROR: " + e.Message);
+            }
 
             return services;
         }
