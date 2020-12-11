43beff71fe852683769004da70b99d988ecace42
Maes, Jeroen <SIDJEMS@sidmar.be>
2020-12-11 08:42:21 +0100

    Pump packages

--- src/EpMon/Infrastructure/CachedTokenService.cs
+++ src/EpMon/Infrastructure/CachedTokenService.cs
@@ -82,6 +82,7 @@ namespace EpMon.Infrastructure
                 _cachedTokenExpiration = DateTime.MinValue;
                 _cachedToken = string.Empty;
 
+                //_logger.LogError($"Error requesting token : ({tokenResponse?.Error} - {tokenResponse?.Exception.InnerException?.Message})");
                 _logger.LogError($"Error requesting token : ({tokenResponse?.ErrorDescription} - {tokenResponse?.HttpErrorReason})");
             }
             else
